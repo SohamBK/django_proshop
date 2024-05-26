a274a560be4cb653844bf9dbbbca64ae5249dd91
SohamBK <balekarsoham@gmail.com>
2024-05-27 00:13:51 +0530

    login and register

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -5,5 +5,8 @@ urlpatterns =[
     path("", views.getRoutes, name="routes"),
     path('products/', views.getProducts, name='products'),
     path('product/<str:pk>/', views.getProduct, name='product'),
-    path('user/login/', views.MyTokenObtainPairSerializer, name='token_obtain_pair'),
+    path('user/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('user/register/', views.registerUser, name='register'),
+    path('user/profile/', views.getUserProfile, name='user-profile'),
+    path('users/', views.getUsers, name='users'),
 ]
